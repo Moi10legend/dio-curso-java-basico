71dda46bc2c522a7e528b5b3f8c6f2b296dba0db
Moi10legend <moisesflopesf@gmail.com>
2023-02-20 20:41:41 -0300

    Operador unario

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,21 +2,16 @@ public class Operadores{
 
     public static void main(String []args){
 
-    //Operadores aula 2
+    //Operadores aula 3
+    //Unário
+    float troco = 2.5F;
 
-    int a = 5; //Operador de atribuiçao
-    int b = a + 3; //operador arietmétrico de soma
-    int c = b - a; //operador arietmétrico de subtraçao
-    int d = 4 * 8; //operador arietmétrico de multiplicaçao
-    int e = 8 / 2; //operador arietmétrico de divisao
-    int f = 18 % 3; //operador arietmétrico de módulo
+    System.out.print( --troco);
+    System.out.println(-troco);
 
-   System.out.println(a);
-   System.out.println(b);
-   System.out.println(c);
-   System.out.println(d);
-   System.out.println(e);
-   System.out.println(f);
+    boolean unarioFalso = (troco == 1.5) ? true : false;
+    System.out.println(!unarioFalso);
+    
 }
 
 }
