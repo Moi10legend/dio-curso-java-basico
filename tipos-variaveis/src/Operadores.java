4b54994e8c9d2b8db4057ac28172d97e5932c54e
Moi10legend <moisesflopesf@gmail.com>
2023-02-20 20:21:46 -0300

    arquivo de operadores de atribuiçao e arietmetrico

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,22 @@
+public class Operadores{
+
+    public static void main(String []args){
+
+    //Operadores aula 2
+
+    int a = 5; //Operador de atribuiçao
+    int b = a + 3; //operador arietmétrico de soma
+    int c = b - a; //operador arietmétrico de subtraçao
+    int d = 4 * 8; //operador arietmétrico de multiplicaçao
+    int e = 8 / 2; //operador arietmétrico de divisao
+    int f = 18 % 3; //operador arietmétrico de módulo
+
+   System.out.println(a);
+   System.out.println(b);
+   System.out.println(c);
+   System.out.println(d);
+   System.out.println(e);
+   System.out.println(f);
+}
+
+}
