c47726a03e65c92ccb751e13bde8356690c1b0bc
Moi10legend <moisesflopesf@gmail.com>
2023-02-20 17:29:14 -0300

    mudança de nome da classe para TiposVariaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,26 @@
+public class TiposVariaveis {
+    public static void main(String[] args)  {
+       
+        //Aula tipos e variáveis 3
+        //Primitive Types
+
+        byte idade = 10;
+        short numero2 = 3000;
+        int numero3 = 2000000;
+        long numero4 = 35L;
+        float numero5 = 3.14F;
+        double numero_6 = 2500.07;
+        // + boolean e char.
+    
+        //Aula tipos e variáveis 4
+
+        final float PI = 3.15F;
+        final String BR = "Brasil";
+
+    
+
+
+
+
+    }
+}
