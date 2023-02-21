ea69ef3b2563df245ad4aab376f74c42a5373c9b
Moi10legend <moisesflopesf@gmail.com>
2023-02-20 21:25:40 -0300

    operador logico

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,19 +2,18 @@ public class Operadores{
 
     public static void main(String [] args){
 
-    //Operadores aula 6
-    //Relacional
+    //Operadores aula 7
+    //Lógico
 
     int a = 1;
     int b =2;
 
-   boolean simNao = (a == b) ? true : false;
-   boolean simNao2 = (b >a ) ? true : false;
-   int resultado = (a <= b) ? 1 : 0;
-   System.out.println(simNao);
-   System.out.println(simNao2);
-   System.out.println(resultado);
-   
+  if ((a <= b) && (b > a) ){
+    System.out.println("as duas afirmaçoes sao verdadeiras");
+  }
+  if ((a == b) || (a != b)){
+    System.out.println("uma das afirmaçoes é verdadeira");
+  }
 
     
     
