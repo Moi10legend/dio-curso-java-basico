26778dea14506702eef30d2db8eaa4cdb7b0f788
Moi10legend <moisesflopesf@gmail.com>
2023-02-22 12:21:06 -0300

    Operador unario

---
+++ Operadores/src/OperadorUnário.java
@@ -0,0 +1,18 @@
+ public class OperadorUnário {
+ 
+    public static void main (String [] args) {
+
+    int numero = 3;
+    numero = - 3;
+
+    String concatenando = "Moisés" + "Freire";
+    boolean falsoParaVerdadeiro = !false;
+    
+    System.out.println(numero--);
+    System.out.println(numero);
+    System.out.println(falsoParaVerdadeiro);
+    System.out.println(concatenando);
+
+    }
+
+}
