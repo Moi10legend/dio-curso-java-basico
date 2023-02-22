2da35f37c190cbf6e81b491aef053a80a66d1755
Moi10legend <moisesflopesf@gmail.com>
2023-02-22 11:58:23 -0300

    Operadores arietmétricos e de atribuiçao

---
+++ Operadores/src/OperadorAtribuiçaoArietmétrico.java
@@ -0,0 +1,13 @@
+    public class OperadorAtribuiçaoArietmétrico{
+
+ public static void main(String [] args) {
+
+    int atribuiçao = 5; 
+    int soma = atribuiçao + 3;
+    int multiplicaçao = 4 * 12;
+    int divisao = 5 / 5;
+    int modulo = 19 % 3;
+
+    System.out.println(modulo);
+ }
+ }
